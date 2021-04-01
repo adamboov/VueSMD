5affe3233f9a8bc9ea3f662574552a233f9b8d56
adamboov <48423629+adamboov@users.noreply.github.com>
2021-04-02 00:30:48 +0800

    添加eslint 父子组件传值以及v-for 迭代数据

--- adam-app/src/App.vue
+++ adam-app/src/App.vue
@@ -1,14 +1,34 @@
 <template>
   <div id="app">
-    <Banner/>
+    <div style="width: 1080px; margin: 0px auto">
+      <Banner :banners="banners" />
+    </div>
   </div>
 </template>
 
 <script>
-import Banner from "./components/Banner"
+import Banner from "./components/Banner";
 export default {
   components: {
-    Banner
+    Banner,
+  },
+  data() {
+    return {
+      banners: [
+        {
+          url: "https://dark-star.top/images/pic/1.jpg",
+          link: "https://dark-star.top/images/pic/1.jpg",
+        },
+        {
+          url: "https://dark-star.top/images/pic/2.jpg",
+          link: "https://dark-star.top/images/pic/2.jpg",
+        },
+        {
+          url: "https://dark-star.top/images/pic/3.jpg",
+          link: "https://dark-star.top/images/pic/3.jpg",
+        },
+      ],
+    };
   },
 };
 </script>
--- adam-app/src/components/Banner.vue
+++ adam-app/src/components/Banner.vue
@@ -1,14 +1,73 @@
 <template>
-  <h1>轮播图</h1>
+  <div class="banner-container">
+    <ul class="images" style="width: 300%">
+      <li v-for="(item, index) in banners" :key="index">
+        <a :href="item.link"><img :src="item.url" alt="" /></a>
+      </li>
+    </ul>
+    <ul class="dots">
+      <li class="active"></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default{
-  name: "Banner",
+export default {
+  props: {
+    banners: {
+      //  属性类型必须是数组
+      type: Array,
+      //  必须要传递该属性类型
+      required: true,
+    },
+  },
 };
-
 </script>
 
 <style>
+.banner-container {
+  height: 350px;
+  position: relative;
+  overflow: hidden;
+}
 
+.banner-container li {
+  display: block;
+  width: 1080px;
+  height: 100%;
+  float: left;
+}
+
+.images {
+  height: 100%;
+  transition: 0.5s;
+}
+
+.banner-container img {
+  width: 1080px;
+  height: 100%;
+}
+
+.dots {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  display: flex;
+}
+
+.dots li {
+  width: 10px;
+  cursor: pointer;
+  height: 10px;
+  margin: 0 3px;
+  border-radius: 50%;
+  border: 1px solid;
+  color: #fff;
+}
+
+.dots li.active {
+  background: #fff;
+}
 </style>
