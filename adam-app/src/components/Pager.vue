bc856cae5f6a5309e25c11639f0a60931b6d7115
xiaolinfeng <1123387544@qq.com>
2022-04-08 18:28:51 +0800

    pager组件创建

---
+++ adam-app/src/components/Pager.vue
@@ -0,0 +1,45 @@
+<template></template>
+
+<script>
+export default {
+  name: "Pager",
+  props: {
+    page: {
+      // 当前页面
+      default: 1,
+      type: Number,
+    },
+    total: {
+      default: 0,
+      type: Number,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pager {
+  text-align: center;
+  margin: 20px 0;
+}
+
+.pager .pager-item {
+  display: inline-block;
+  padding: 5px 7px;
+  border: 1px solid #ccc;
+  margin: 8px;
+  cursor: pointer;
+  color: rgb(96, 96, 244);
+}
+
+.pager .pager-item.disabled {
+  color: #ccc;
+  cursor: not-allowed;
+}
+
+.pager .pager-item.active {
+  color: #f40;
+  border: none;
+  cursor: auto;
+}
+</style>
